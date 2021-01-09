75132288b88488bb4119913ac66d14de828e1356
Jack Koontz <a66062@gmail.com>
2021-01-09 15:31:24 -0600

    signup/login

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Root from "./Root";
+import { Route, Switch  } from "react-router-dom";
+import Home from "./components/Home"; 
+import Login from "./components/login/Login";
+import Dashboard from "./components/dashboard/Dashboard";
+import Signup from "./components/signup/Signup";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+class App extends Component {
+  render() {
+    return (
+    <div>
+      <Root>
+        <Switch>
+          <Route exact path ="/" component={Home}/>
+          <Route path = "/login" component={Login}/>
+          <Route path = "/dashboard" component={Dashboard}/>
+          <Route path = "/signup" component={Signup}/>
+        </Switch>
+      </Root>
     </div>
-  );
+    );
+  }
 }
 
 export default App;
